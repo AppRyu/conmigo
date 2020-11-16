e850b4b4a70c76fc25e6cb71df7de7c91dfcdba1
AppRyu <birdofnight123@gmail.com>
2020-11-17 00:47:31 +0900

    hero作成

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,5 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js").sass(
     "resources/sass/app.scss",
-    "public/css",
-    {
-        includePaths: ["node_modules", "node_modules/bootstrap-honoka/scss"]
-    }
+    "public/css"
 );
