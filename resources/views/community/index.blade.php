799366d5d545cab4f2ed8c55f35d04b4c0530de7
AppRyu <birdofnight123@gmail.com>
2020-12-07 18:57:32 +0900

    コミュニティ詳細ページレイアウト作成

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -14,9 +14,8 @@
                     <h3 class="community-name">{{ $community->name }}</h3>
                 </a>
                 <div class="community-times">
-                    <div class="community-time">
-                        <span class="community-time__head community-time__head--start">開始日時</span><span class="d-inline-block">{{ $community->getDateAndTime($community->start_time) }}</span></div>
-                    <div class="community-time"><span class="community-time__head community-time__head--end">終了日時</span><span class="d-inline-block">{{ $community->getDateAndTime($community->end_time) }}</span></div>
+                    <div class="community-time"><span class="community-time__head community-time__head--start">開始日時</span><span class="d-inline-block">{{ $community->getDate($community->start_time) }}</span><span class="d-inline-block">{{ $community->getTime($community->start_time) }}</span></div>
+                    <div class="community-time"><span class="community-time__head community-time__head--end">終了日時</span><span class="d-inline-block">{{ $community->getDate($community->end_time) }}</span><span class="d-inline-block">{{ $community->getTime($community->end_time) }}</span></div>
                 </div>
                 <div class="community-row">
                     <div class="community-requested">応募数<span class="community-requested__num">3</span>件</div>
--- resources/views/community/show.blade.php
+++ resources/views/community/show.blade.php
@@ -8,14 +8,21 @@
 		<tbody>
 			<tr class="comm-det-tb__tr">
 				<th class="comm-det-tb__th" scope="row">開始日時</th>
-				<td class="comm-det-tb__td"><span class="d-inline-block">{{ $startDay }}</span><span class="d-inline-block">{{ $startTime }}</span></td>
+				<td class="comm-det-tb__td"><span class="comm-det-tb__td_d-inline-block">{{ $startDay }}</span><span class="comm-det-tb__td_d-inline-block">{{ $startTime }}</span></td>
 			</tr>
 			<tr class="comm-det-tb__tr">
 				<th class="comm-det-tb__th" scope="row">終了日時</th>
-				<td class="comm-det-tb__td"><span class="d-inline-block">{{ $endDay }}</span><span class="d-inline-block">{{ $endTime }}</span></td>
+				<td class="comm-det-tb__td"><span class="comm-det-tb__td_d-inline-block">{{ $endDay }}</span><span class="comm-det-tb__td_d-inline-block">{{ $endTime }}</span></td>
 			</tr>
 		</tbody>
 	</table>
+	<div class="comm-det-desc">
+		<h3 class="comm-det-desc__hd"><span class="comm-det-desc__hd_emphasis">コミュニティの詳細</span></h3>
+		<div class="comm-det-desc__cmt">{{ $community->detail }}</div>
+	</div>
+	<div class="comm-det-rct">
+		<a class="comm-det-rct__btn" href="">応募する</a>
+	</div>
 @endsection
     
 @section('sidebar')
