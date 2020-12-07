e7b6f794f96fb16e1952a67924671a016ef846f9
AppRyu <birdofnight123@gmail.com>
2020-12-07 21:51:09 +0900

    カラム変更マイグレーションファイル追加

---
+++ database/migrations/2020_12_07_211512_rename_communities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameCommunitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('communities', function (Blueprint $table) {
+            $table->renameColumn('start', 'start');
+            $table->renameColumn('end', 'end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('communities', function (Blueprint $table) {
+            $table->renameColumn('start', 'start');
+            $table->renameColumn('end', 'end');
+        });
+    }
+}
