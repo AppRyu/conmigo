02664dc50a57f09595b4a6c8b3f0596c1ea38f07
AppRyu <birdofnight123@gmail.com>
2020-12-19 12:55:55 +0900

    stylelint導入

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        "stylelint-config-recommended-scss",
+        "stylelint-prettier/recommended"
+    ],
+    rules: {
+        'color-hex-case': 'lower',
+        'color-hex-length': 'short',
+    }
+}
