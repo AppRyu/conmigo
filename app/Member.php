d64c49c4d1b106a4691b2339b8ab7502894ddfcb
AppRyu <birdofnight123@gmail.com>
2021-01-01 18:02:16 +0900

    create new model file

---
+++ app/Member.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    protected $fillable = [
+        'user_id', 'community_id'
+    ];
+
+    public function users() 
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    public function communities() 
+    {
+        return $this->belongsTo('App\Community', 'community_id', 'id');
+    }
+}
