25b3041105289c137819e95f0b3992621e5b5344
AppRyu <birdofnight123@gmail.com>
2021-01-03 20:48:46 +0900

    シングルクウォーテーションに変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,14 @@
-import "./bootstrap";
-import Vue from "vue";
-import CommunitySetTimeInput from "./components/CommunitySetTimeInput.vue";
-import CommunityRecruitBtn from "./components/CommunityRecruitBtn.vue";
-import CommunitySelectUser from "./components/CommunitySelectUser.vue";
-
+import './bootstrap';
+import Vue from 'vue';
+import CommunitySetTimeInput from './components/CommunitySetTimeInput.vue';
+import CommunityRecruitBtn from './components/CommunityRecruitBtn.vue';
+import CommunitySelectUser from './components/CommunitySelectUser.vue';
 
 const app = new Vue({
-    el: "#app",
+    el: '#app',
     components: {
         CommunitySetTimeInput,
         CommunityRecruitBtn,
-        CommunitySelectUser
-    }
+        CommunitySelectUser,
+    },
 });
