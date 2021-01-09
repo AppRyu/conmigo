53f28ee4822f46dace44a873f5b96cd72accdc69
AppRyu <birdofnight123@gmail.com>
2021-01-09 17:10:38 +0900

    チャットページindexレイアウト作成

--- resources/views/front/chat/index.blade.php
+++ resources/views/front/chat/index.blade.php
@@ -3,7 +3,25 @@
 @section('title', 'チャットルーム一覧')
 
 @section('content')
-    <h1>チャットルーム一覧</h1>
+    <section>
+        <h2 class="page-tit u-mb-xl"><i class="fas fa-envelope u-mr-base"></i>メッセージ
+            <div class="u-fs-sm u-fw-normal u-mt-xs">コミュニティのメッセージ一覧です。</div>
+        </h2>
+        <div>
+            @foreach ($communities as $community)
+            <a class="msg-link" href="{{ route('chat.show', ['community' => $community]) }}">
+                <section class="msg-sec u-p-base">
+                    <h3 class="msg-sec__tit u-fs-lg">{{ $community->name }}</h3>
+                    <div class="msg-content">
+                        <div class="msg-content__date-time u-fw-bold u-mb-xs u-md-mr-base u-md-mb-no"><span class="c-tag-sm c-tag-red u-mr-sm">開始日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->start) }} {{ $community->getTime($community->start) }}</span></div>
+                        <div class="msg-content__date-time u-fw-bold"><span class="c-tag-sm c-tag-green u-mr-sm">終了日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->end) }} {{ $community->getTime($community->end) }}</span></div>
+                    </div>
+                </section>
+            </a>
+            @endforeach
+        </div>
+    </section>
+
 @endsection
     
 @section('sidebar')
