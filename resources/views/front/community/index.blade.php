de8d562cabb843be66cf7ea4902c06e4f9e54400
AppRyu <birdofnight123@gmail.com>
2021-01-10 21:14:50 +0900

    コミュニティ一覧ページ作成ユーザーの画像表示&レイアウト修正

--- resources/views/front/community/index.blade.php
+++ resources/views/front/community/index.blade.php
@@ -3,33 +3,38 @@
 @section('title', 'コミュニティ一覧')
 
 @section('content')
-    <div class="community-container">
+    <section>
         <h2 class="page-tit u-mb-xl"><i class="fas fa-search u-mr-base"></i>コミュニティを探す
             <div class="u-fs-sm u-fw-normal u-mt-xs">一緒に学習するコミュニティを探してください。</div>
         </h2>
         @foreach($communities as $community)
-        <div class="community-content">
-            <div class="community-content__left">
-                <div class="community-flg">募集中</div>
-            </div>
-            <div class="community-content__right">
-                <h3 class="community-name">
-                    <a class="community-name__link" href="{{ route('community.show', ['community' => $community]) }}">
-                        {{ $community->name }}
-                    </a>
-                </h3>
-                <div class="community-times">
-                    <div class="community-time"><span class="community-time__tag community-time__tag--start">開始日時</span><span class="d-inline-block">{{ $community->getDate($community->start) }} {{ $community->getTime($community->start) }}</span></div>
-                    <div class="community-time"><span class="community-time__tag community-time__tag--end">終了日時</span><span class="d-inline-block">{{ $community->getDate($community->end) }} {{ $community->getTime($community->end) }}</span></div>
+        <a class="community-content__link" href="{{ route('community.show', ['community' => $community]) }}">
+            <div class="community-content u-xs-d-flex">
+                <div class="community-content__left u-xs-mr-base">
+                    <div class="community-flg">募集中</div>
                 </div>
-                <div class="community-row">
-                    <div class="community-requested">応募数<span class="community-requested__num">{{ $community->getTotalRecruitment() }}</span>件</div>
-                    <div class="community-created-user"><a class="community-created-user__link" href="{{ route('user.show', ['user_name' => $community->users->user_name]) }}">{{ $community->users->user_name }}</a></div>
-                </div>
+                <section class="community-content__right">
+                    <h3 class="community-name u-fs-lg">{{ $community->name }}</h3>
+                    <div class="u-md-d-flex">
+                        <div class="u-fw-bold u-mb-xs u-md-mr-base u-md-mb-no"><span class="c-tag-sm c-tag-red u-mr-sm">開始日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->start) }} {{ $community->getTime($community->start) }}</span></div>
+                        <div class="u-fw-bold"><span class="c-tag-sm c-tag-green u-mr-sm">終了日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->end) }} {{ $community->getTime($community->end) }}</span></div>
+                    </div>
+                    <div class="community-row">
+                        <div class="u-fs-sm">応募数<span class="community-requested__num u-fs-xl u-mx-sm">{{ $community->getTotalRecruitment() }}</span>件</div>
+                        <div class="u-d-flex">
+                            @if ($community->users->profile_image)
+                            <div class="c-icon-xs u-mr-xs"><img src="{{ asset('/storage/img/'.$community->users->profile_image) }}" alt="コミュニティ作成したユーザー画像"></div>
+                            @else
+                            <div class="c-icon-xs u-mr-xs"><img src="{{ asset('./img/default-icon.png') }}" alt="コミュニティ作成したユーザー画像"></div>
+                            @endif
+                            <a class="community-created-user__link" href="{{ route('user.show', ['user_name' => $community->users->user_name]) }}">{{ $community->users->user_name }}</a>
+                        </div>
+                    </div>
+                </section>
             </div>
-        </div>
+        </a>
         @endforeach
-    </div>
+    </section>
 @endsection
     
 @section('sidebar')
