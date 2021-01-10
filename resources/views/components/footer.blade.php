bfff0c0fe28fce5ea7adf3a52ee4500ae769f7c8
AppRyu <birdofnight123@gmail.com>
2021-01-11 03:50:04 +0900

    footerスタイル修正

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,3 @@
 <footer class="footer">
-    <div class="container">
-        <p class="copyright">© 2020 AppRyu</p>        
-    </div>
+    <p class="u-mb-no u-py-base u-ta-center"><small>© 2021 AppRyu</small></p>
 </footer>
