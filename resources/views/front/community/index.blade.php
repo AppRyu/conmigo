44f7544032ed25c736297d50bcd46ce1c8bef892
AppRyu <birdofnight123@gmail.com>
2021-01-12 00:41:13 +0900

    コミュニティ一覧ページレイアウト修正

--- resources/views/front/community/index.blade.php
+++ resources/views/front/community/index.blade.php
@@ -11,17 +11,30 @@
             @foreach($communities as $community)
             <a class="community-content__link" href="{{ route('community.show', ['community' => $community]) }}">
                 <div class="community-content u-xs-d-flex">
-                    <div class="community-content__left u-xs-mr-base">
-                        <div class="community-flg">募集中</div>
+                    <div class="community-content__left u-d-none u-xs-d-block u-xs-mr-base">
+                        @if($community->isPast($community->start))
+                        <div class="community-badge c-badge-base c-badge-red">締切</div>
+                        @elseif($community->members->where('community_id', $community->id)->count())
+                        <div class="community-badge c-badge-base c-badge-blue">満員</div>
+                        @else
+                        <div class="community-badge c-badge-base c-badge-green">募集中</div>
+                        @endif
                     </div>
+                    @if($community->isPast($community->start))
+                    <div class="community-badge-sm community-badge-sm--red u-d-block u-xs-d-none"><span class="community-badge-sm__txt">締切</span></div>
+                    @elseif($community->members->where('community_id', $community->id)->count())
+                    <div class="community-badge-sm community-badge-sm--blue u-d-block u-xs-d-none"><span class="community-badge-sm__txt">満員</span></div>
+                    @else
+                    <div class="community-badge-sm community-badge-sm--green u-d-block u-xs-d-none"><span class="community-badge-sm__txt">募集中</span></div>
+                    @endif
                     <section class="community-content__right">
-                        <h3 class="community-name u-fs-lg">{{ $community->name }}</h3>
+                        <h3 class="u-c---dark-blue u-fs-lg u-ml-base u-xs-ml-no">{{ $community->name }}</h3>
                         <div class="u-md-d-flex">
                             <div class="u-fw-bold u-mb-xs u-md-mr-base u-md-mb-no"><span class="c-tag-sm c-tag-red u-mr-sm">開始日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->start) }} {{ $community->getTime($community->start) }}</span></div>
                             <div class="u-fw-bold"><span class="c-tag-sm c-tag-green u-mr-sm">終了日時</span><span class="d-inline-block u-fs-sm">{{ $community->getDate($community->end) }} {{ $community->getTime($community->end) }}</span></div>
                         </div>
                         <div class="community-row">
-                            <div class="u-fs-sm">応募数<span class="community-requested__num u-fs-xl u-mx-sm">{{ $community->getTotalRecruitment() }}</span>件</div>
+                            <div class="u-fs-sm">応募数<span class="u-c---dark-blue u-fs-xl u-mx-sm">{{ $community->getTotalRecruitment() }}</span>件</div>
                             <div class="u-d-flex">
                                 @if ($community->users->profile_image)
                                 <div class="c-icon-xs u-mr-xs"><img src="{{ asset('/storage/img/'.$community->users->profile_image) }}" alt="コミュニティ作成したユーザー画像"></div>
