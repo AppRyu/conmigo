3875254388f7706a0a18fde590d3efeec448efe9
AppRyu <birdofnight123@gmail.com>
2021-01-15 18:11:21 +0900

    timeTableをモジュール化

---
+++ resources/views/modules/timeTable.blade.php
@@ -0,0 +1,12 @@
+<table class="time-table">
+    <tbody>
+        <tr class="time-table__tr">
+            <th class="time-table__th time-table__th--start" scope="row">開始日時</th>
+            <td class="time-table__td"><span class="u-d-inline-block u-mr-sm">{{ $startDate }}</span><span class="u-d-inline-block">{{ $startTime }}</span></td>
+        </tr>
+        <tr class="time-table__tr">
+            <th class="time-table__th time-table__th--end" scope="row">終了日時</th>
+            <td class="time-table__td"><span class="u-d-inline-block u-mr-sm">{{ $endDate }}</span><span class="u-d-inline-block">{{ $endTime }}</span></td>
+        </tr>
+    </tbody>
+</table>
