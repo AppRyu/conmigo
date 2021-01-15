d3cc94f42eea4010c47f949d3bdb301784114fb3
AppRyu <birdofnight123@gmail.com>
2021-01-15 18:19:35 +0900

    chat.showレイアウト修正

--- resources/views/front/chat/show.blade.php
+++ resources/views/front/chat/show.blade.php
@@ -7,16 +7,29 @@
         <h2 class="page-tit u-mb-xl"><i class="fas fa-comments u-mr-base"></i>チャットルーム
             <div class="u-fs-sm u-fw-normal u-mt-xs">コミュニティに関する連絡はこちらのページをご利用ください。</div>
         </h2>
-        @switch($communityStatus)
+        <div>
+            @if(is_null($community->deleted_at))
+            <h3 class="u-fs-xl u-mb-base"><a class="c-link-blue" href="{{ route('community.show', ['community' => $community]) }}">{{ $community->name }}</a></h3>
+            @else
+            <h3 class="u-fs-xl u-mb-base">{{ $community->name }}</h3>
+            @endif
+            @switch($communityStatus)
             @case('deleted')
-                <p class="c-alert-red u-mb-base">このコミュニティは既に削除されました。</p>
-                @break
+                <div class="c-alert-red u-mb-base">このコミュニティは既に削除されました。</div>
+            @break
+            @case('noMember')
+                <div class="c-alert-yellow u-mb-base">コミュニティ管理画面からメンバーを確定しよう。</div>
+            @break
             @case('noMsg')
-                <p class="c-alert-green u-mb-base">メンバーに挨拶メッセージを送ろう。</p>
-                @break
+                <div class="c-alert-green u-mb-base">メンバーに挨拶メッセージを送ろう。</div>
+            @break
             @default
-                <p class="c-alert-gray u-mb-base">一緒に勉強する時間や方法（slack,zoom等）を話し合ってください。</p>
-        @endswitch
+                <div class="c-alert-gray u-mb-base">一緒に勉強する時間や方法（slack,zoom等）を話し合ってください。</div>
+            @endswitch
+            <div class="u-mb-lg">
+                @include('./modules/timeTable', ['startDate' => $community->getDate($community->start), 'startTime' => $community->getTime($community->start), 'endDate' => $community->getDate($community->end), 'endTime' => $community->getTime($community->end)])
+            </div>
+        </div>
         <div class="chat-container u-px-base u-pt-base">
             @foreach($AllCommunityMember as $user)
             @continue(Auth::user()->id === $user->id)
@@ -29,7 +42,7 @@
                     @endif
                 </div>
                 <section>
-                    <h3 class="u-fs-lg"><a href="{{ route('user.show', ['user_name' => $user->user_name]) }}">{{ $user->user_name }}</a></h3>
+                    <h4 class="u-fs-lg"><a class="c-link-blue" href="{{ route('user.show', ['user_name' => $user->user_name]) }}">{{ $user->user_name }}</a></h4>
                     <!--  TODO 最終ログイン時間を表示させる 下記例 -->
                     <!-- <p>最終ログイン：2日前</p> -->
                 </section>
@@ -47,9 +60,9 @@
                 <section class="chat-content__box">
                     <div class="chat-content__row u-mb-sm">
                         @if(Auth::user()->id === $chat->user_id)
-                        <h3 class="u-fs-base u-fw-xbd">{{ $chat->userWithTrashed($chat->user_id)->user_name }}</h3>
+                        <h4 class="u-fs-base u-fw-xbd">{{ $chat->userWithTrashed($chat->user_id)->user_name }}</h4>
                         @else
-                        <h3 class="u-fs-base">{{ $chat->userWithTrashed($chat->user_id)->user_name }}</h3>
+                        <h4 class="u-fs-base">{{ $chat->userWithTrashed($chat->user_id)->user_name }}</h4>
                         @endif
                         <p class="chat-content__date">{{ $chat->created_at }}</p>
                     </div>
