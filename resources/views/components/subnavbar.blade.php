ce0c1c607fda973507e14f09ca072ad9e8bcfb95
AppRyu <birdofnight123@gmail.com>
2021-01-17 00:32:47 +0900

    テキスト修正

--- resources/views/components/subnavbar.blade.php
+++ resources/views/components/subnavbar.blade.php
@@ -2,10 +2,10 @@
     <div class="container">
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('community.index') }}">コミュニティをさがす</a>
+                <a class="nav-link" href="{{ route('community.index') }}">コミュニティを探す</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('community.create') }}">コミュニティをつくる</a>
+                <a class="nav-link" href="{{ route('community.create') }}">コミュニティを作成</a>
             </li>
         </ul>
     </div>
